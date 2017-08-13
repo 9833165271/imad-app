a74cbea59287f65f9ec1c4aa66188ea8bbf4f6c4
9833165271 <30731768+9833165271@users.noreply.github.com>
2017-08-13 11:59:54 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'photo_1.jpg'));
+  res.sendFile(path.join(__dirname, 'imad-app', 'photo_1.jpg'));
 });
 
 
