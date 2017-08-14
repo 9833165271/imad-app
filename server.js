6d65d78a014878376b88bb87f447baed2bee4fff
9833165271 <30731768+9833165271@users.noreply.github.com>
2017-08-14 10:15:54 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/article.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
